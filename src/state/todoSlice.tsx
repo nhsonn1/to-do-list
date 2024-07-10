878b4a3db4c869b0b763bfe2366b9b1769f6eb5f
sonnh139 <sonnh139@fpt.com>
2024-07-10 15:20:51 +0700

    Added slice and store to state folder

---
+++ src/state/todoSlice.tsx
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Todo {
+    id: string;
+    name: string;
+    completed: boolean;
+}
+
+const initialState: Todo[] = [];
+
+const todoSlice = createSlice({
+    name: "todos",
+    initialState,
+    reducers: {
+        addTask: (state, action: PayloadAction<Todo>) => {
+            state.push(action.payload);
+        },
+        toggleTaskCompletion: (state, action: PayloadAction<string>) => {
+            const todo = state.find(todo => todo.id === action.payload);
+            if (todo) {
+                todo.completed = !todo.completed;
+            }
+        },
+        deleteTask: (state, action: PayloadAction<string>) => {
+            return state.filter(todo => todo.id !== action.payload);
+        },
+        updateTask: (state, action: PayloadAction<{id: string; name: string}>) => {
+            const {id, name} = action.payload;
+            const todo = state.find(todo => todo.id === id);
+            if (todo) {
+                todo.name = name;
+            }
+        }
+    }
+});
+
+export const { addTask, deleteTask, toggleTaskCompletion, updateTask } = todoSlice.actions;
+export default todoSlice.reducer;
