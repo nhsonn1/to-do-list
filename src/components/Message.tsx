72ac7317c77ff40e2263a65fb5270d3134818b9d
sonnh139 <sonnh139@fpt.com>
2024-07-10 16:17:39 +0700

    Added styled message

---
+++ src/components/Message.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const TasksMessage = styled.div`
+  margin-top: 20px;
+  padding: 20px;
+  text-align: center;
+  background-color: #f8f9fa;
+  border: 1px solid #dee2e6;
+  border-radius: 5px;
+  font-weight: bold;
+  color: #ff9100;
+`;
+
+export default TasksMessage;
